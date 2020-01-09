f747b157d51700a0cb04b40158ac4dcd2465a1fe
lvckylvkas <1911034@stud.hs-mannheim.de>
2020-01-09 14:20:11 +0100

    push

--- src/Test.java
+++ src/Test.java
@@ -1,8 +1,33 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class Test {
+import javax.swing.JFrame;
+import javax.swing.JPasswordField;
+
+public class Test extends JFrame{
+	
+	JPasswordField j;
+	
+	public Test() {
+		this.setVisible(true);
+		j = new JPasswordField();
+		add(j);
+		j.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				// TODO Auto-generated method stub
+				System.out.println(j.getText());
+				
+			}
+		});
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.pack();
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		new Test();
 
 	}
 
