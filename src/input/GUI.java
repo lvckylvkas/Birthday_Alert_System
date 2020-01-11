19e8c465f652889790509ac58157b0e4a5d2192e
lvckylvkas <1911034@stud.hs-mannheim.de>
2020-01-11 19:00:56 +0100

    updates

--- src/input/GUI.java
+++ src/input/GUI.java
@@ -1,18 +1,25 @@
 package input;
 
 import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.InputStreamReader;
 
-import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 
-
-
-public class GUI extends JFrame {
+public class GUI extends JFrame implements ActionListener {
 
 	JLabel info;
 	InputFields input;
 	SubmitButton submit;
+	FileWriter fw;
+	BufferedReader fr;
 
 	public GUI(String s) {
 		super(s);
@@ -21,11 +28,57 @@ public class GUI extends JFrame {
 		info = new JLabel("Insert Name and Birthday(dd.mm):");
 		add(info, BorderLayout.NORTH);
 		input = new InputFields();
+		input.birthday.text.addActionListener(this);
 		add(input, BorderLayout.CENTER);
 		submit = new SubmitButton();
+		submit.button.addActionListener(this);
+
 		add(submit, BorderLayout.SOUTH);
 		this.pack();
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if (!input.birthday.text.getText().matches("\\d\\d\\.\\d\\d")) {
+			submit.label.setText("invalid date format");
+			submit.label.setVisible(true);
+		} else {
+			String text = readCsv();
+			text += input.name.text.getText() + ";" + input.birthday.text.getText() + "\r\n";
+			writeCsv(text);
+			submit.label.setText("Birthday saved");
+			submit.label.setVisible(true);
+
+		}
+
+	}
+	
+	public String readCsv() {
+		try {
+			fr = new BufferedReader(new FileReader("resources/birthdays.csv"));
+			StringBuilder s = new StringBuilder();
+			String row = "";
+			while ((row = fr.readLine()) != null) {
+				s.append(row);
+				System.out.println(row);
+			}
+			fr.close();
+			return s.toString();
+		} catch (Exception ex) {
+			System.out.println(ex.getMessage());
+		}
+		return null;
+	}
+	
+	public void writeCsv(String s) {
+		try {
+			fw = new FileWriter(new File("resources/birthdays.csv"));
+			fw.append(s);
+			fw.close();
+		} catch (Exception ex) {
+			System.out.println(ex.getMessage());
+		}
 	}
 
 	public static void main(String[] args) {
