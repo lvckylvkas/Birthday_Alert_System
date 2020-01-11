126f2b4b4adabf9f2e2ebfd1ee66efca43392e82
lvckylvkas <1911034@stud.hs-mannheim.de>
2020-01-11 22:27:26 +0100

    completed

---
+++ src/output/Checker.java
@@ -0,0 +1,49 @@
+package output;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+
+public class Checker {
+	
+	BufferedReader fr;
+	
+	public String[] getCsv() {
+		ArrayList<String> list= new ArrayList<>();
+		try {
+			fr = new BufferedReader(new FileReader(new File("resources/birthdays.csv")));
+			String line = "";
+			while((line = fr.readLine()) != null)
+				list.add(line);
+			String[] result = new String[list.size()];
+			for(int i =0; i< list.size(); i++)
+				result[i] = list.get(i);
+			return result;	
+		}
+		catch(Exception e) {
+			return null;
+		}
+	}
+	
+	public ArrayList<String> getBirthdayPeople()  {
+		ArrayList<String> result = new ArrayList<>();
+		Date today = new Date();
+		SimpleDateFormat sdf = new SimpleDateFormat("dd.MM");
+		Date birthday;
+		for(String line: getCsv()) {
+			String[] parted = line.split(";");
+			try {
+			if(sdf.format(today).equals(parted[1])) {
+				result.add(parted[0]);
+			}
+			}
+			catch(Exception e) {
+				System.out.println("First line can't be a date");
+			}
+		}
+		return result;
+	}
+}
--- src/output/GUI.java
+++ src/output/GUI.java
@@ -1,9 +1,37 @@
 package output;
 
-public class GUI {
+import java.awt.Font;
+import java.text.ParseException;
 
-	public static void main(String[] args) {
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.text.Style;
+
+public class GUI extends JFrame{
+	
+	Checker c;
+	
+	public GUI(String s) {
+		super(s);
+		this.setVisible(true);
+		JTextArea output = new JTextArea();
+		Checker c = new Checker();
+		String result = "";
+		for(String name : c.getBirthdayPeople())
+			result += name + " has Birthday!\n";
+		output.setText(result);
+		add(output);
+		output.setFont(new Font(Font.MONOSPACED, Font.BOLD, 50));
+		this.pack();
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		
+	}
+
+	public static void main(String[] args) throws ParseException {
 		// TODO Auto-generated method stub
+		new GUI("Birthday Alert");
 
 	}
 
